3d913f22c9e922494df7e3602273c3c12f90ddc3
Ugo Paternostro <ugo.git@paternostro.org>
2024-02-04 15:46:42 +0100

    Remove debug lines

--- app/widgets/add-comment/dialog-service.js
+++ app/widgets/add-comment/dialog-service.js
@@ -7,14 +7,11 @@ import { AddCommentViewModel } from '~/widgets/add-comment/model';
 var model;
 
 export function onModalOpen(args) {
-// console.log('onModalOpen: ' + args);
     model = args.model;
     model.addComment = new AddCommentViewModel(model);
 }
 
 export function onCommentOkTap(args) {
-// console.log('onCommentOkTap SERVICE');
-// console.log('onCommentOkTap: ' + JSON.stringify(args.object.page.bindingContext.object.attrs.__name, null, 4));
     IcingaFacade.getInstance().commentService(model.object.attrs.__name,
         ApplicationSettings.getString('username'), 
         model.addComment.comment,
@@ -25,7 +22,6 @@ export function onCommentOkTap(args) {
 }
 
 export function onCommentCloseTap(args) {
-// console.log('onCommentCloseTap SERVICE');
     closeModal();
 }
 
@@ -37,14 +33,12 @@ export function addCommentCB(obj) {
 }
 
 export function onDateTap(args) {
-// console.log('onDateTap SERVICE');
     openModal("~/widgets/date-picker/dialog", require("~/widgets/date-picker/dialog"), {
         "model": model,
     });
 }
 
 export function onTimeTap(args) {
-// console.log('onTimeTap SERVICE');
     openModal("~/widgets/time-picker/dialog", require("~/widgets/time-picker/dialog"), {
         "model": model,
     });
